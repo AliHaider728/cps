ba2889f042fd7941c9a33a2a980743fd236cb635
AliHaider728 <decantboy67@gmail.com>
2026-04-16 14:48:58 +0500

    "update env"

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,14 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 if (!supabaseUrl) {
   throw new Error("VITE_SUPABASE_URL is not configured");
 }
 
 if (!supabaseKey) {
-  throw new Error("VITE_SUPABASE_ANON_KEY is not configured");
+  throw new Error("VITE_SUPABASE_PUBLISHABLE_KEY is not configured");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
